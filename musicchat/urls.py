2ce44c9c2d810e25d4666de581b9e5325a2b80f4
fbwinghsa <fb_linux@163.com>
2014-11-03 17:14:41 +0800

    Update urls.py

--- musicchat/urls.py
+++ musicchat/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+import musicchat.view;
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'musicchat.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^service/$', musicchat.view.interface),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
